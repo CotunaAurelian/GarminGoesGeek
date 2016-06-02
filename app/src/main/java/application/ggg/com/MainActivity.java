a334012f514a7cd2290a274afa946e834d017308
Aurelian Cotuna <aurelian.cotuna@garmin.com>
2016-06-02 16:21:01 +0300

    merged changes

--- app/src/main/java/application/ggg/com/MainActivity.java
+++ app/src/main/java/application/ggg/com/MainActivity.java
@@ -1,27 +1,162 @@
 package application.ggg.com;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
+import com.yalantis.contextmenu.lib.MenuObject;
+import com.yalantis.contextmenu.lib.MenuParams;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemClickListener;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemLongClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import application.ggg.com.running.AnimationGamePanel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMenuItemClickListener, OnMenuItemLongClickListener {
+
+    private FragmentManager fragmentManager;
+    private ContextMenuDialogFragment mMenuDialogFragment;
+
+    private Toolbar mToolbar;
+    AnimationGamePanel mGamePanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        initMenuFragment();
+        initToolbar();
+
 
         RelativeLayout containerRunning = (RelativeLayout) findViewById(R.id.containerRunning);
 
-        AnimationGamePanel gamePanel = new AnimationGamePanel(this);
-        gamePanel.setBackground(null);
-        gamePanel.setLayoutParams(new LinearLayout.LayoutParams(170, 170));
-        containerRunning.addView(gamePanel);
+        mGamePanel= new AnimationGamePanel(this);
+        mGamePanel.setBackground(null);
+        mGamePanel.setLayoutParams(new LinearLayout.LayoutParams(170, 170));
 
-        //setContentView(new MainGamePanel(this));
+       // containerRunning.addView(mGamePanel);
+    }
+
+    private void initMenuFragment() {
+        MenuParams menuParams = new MenuParams();
+        menuParams.setActionBarSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics()));
+        menuParams.setAnimationDuration(70);
+        menuParams.setMenuObjects(getMenuObjects());
+        menuParams.setClipToPadding(true);
+        menuParams.setClosableOutside(true);
+        mMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams);
+        mMenuDialogFragment.setItemClickListener(this);
+        mMenuDialogFragment.setItemLongClickListener(this);
+    }
+
+    private List<MenuObject> getMenuObjects() {
+
+
+        List<MenuObject> menuObjects = new ArrayList<>();
+        MenuObject topObject = new MenuObject();
+        topObject.setBgColor(android.R.color.transparent);
+        topObject.setDividerColor(android.R.color.transparent);
+
+        MenuObject walkingObjects = new MenuObject("Walking");
+        walkingObjects.setDrawable(getDrawable(R.drawable.gcm3_profile_activity_icon_walking_on));
+        walkingObjects.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        MenuObject runningObject = new MenuObject("Running");
+        runningObject.setDrawable(getDrawable(R.drawable.gcm3_profile_activity_icon_running_on));
+        runningObject.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        MenuObject cyclingObject = new MenuObject("Cycling");
+        cyclingObject.setDrawable(getDrawable(R.drawable.gcm3_profile_activity_icon_cycling_on));
+        cyclingObject.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        MenuObject multiSports = new MenuObject("Multisport");
+        multiSports.setDrawable(getDrawable(R.drawable.gcm3_profile_activity_icon_triathlon_on));
+        multiSports.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        menuObjects.add(topObject);
+        menuObjects.add(walkingObjects);
+        menuObjects.add(runningObject);
+        menuObjects.add(cyclingObject);
+        menuObjects.add(multiSports);
+
+        return menuObjects;
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView mToolBarTextView = (TextView) findViewById(R.id.text_view_toolbar_title);
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        mToolBarTextView.setText("Garmin Goes Geek");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_menu:
+                if (fragmentManager.findFragmentByTag(ContextMenuDialogFragment.TAG) == null) {
+                    mMenuDialogFragment.show(fragmentManager, ContextMenuDialogFragment.TAG);
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mMenuDialogFragment != null && mMenuDialogFragment.isAdded()) {
+            mMenuDialogFragment.dismiss();
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    public void onMenuItemClick(View clickedView, int position) {
+        Toast.makeText(this, "Clicked on position: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMenuItemLongClick(View clickedView, int position) {
+        Toast.makeText(this, "Long clicked on position: " + position, Toast.LENGTH_SHORT).show();
     }
 }
-
--- app/src/main/java/application/ggg/com/running/AnimationGamePanel.java
+++ app/src/main/java/application/ggg/com/running/AnimationGamePanel.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package application.ggg.com.running;
 
@@ -16,92 +16,90 @@ import application.ggg.com.R;
 
 /**
  * @author impaler
- * This is the main surface that handles the ontouch events and draws
- * the image to the screen.
+ *         This is the main surface that handles the ontouch events and draws
+ *         the image to the screen.
  */
-public class AnimationGamePanel extends SurfaceView implements
-		SurfaceHolder.Callback {
+public class AnimationGamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
-	private static final String TAG = AnimationGamePanel.class.getSimpleName();
-	
-	private MainThread thread;
-	private StickmanAnimated elaine;
+    private static final String TAG = AnimationGamePanel.class.getSimpleName();
 
-	// the fps to be displayed
-	private String avgFps;
-	public void setAvgFps(String avgFps) {
-		this.avgFps = avgFps;
-	}
+    private MainThread thread;
+    private StickmanAnimated elaine;
 
-	public AnimationGamePanel(Context context) {
-		super(context);
-		// adding the callback (this) to the surface holder to intercept events
-		getHolder().addCallback(this);
+    // the fps to be displayed
+    private String avgFps;
 
-		// create Elaine and load bitmap
-		elaine = new StickmanAnimated(
-				BitmapFactory.decodeResource(getResources(), R.mipmap.running)
-				, 10, 50	// initial position
-				, 123, 128	// width and height of sprite
-				, 20, 7);	// FPS and number of frames in the animation
-		
-		// create the game loop thread
-		thread = new MainThread(getHolder(), this);
-		
-		// make the GamePanel focusable so it can handle events
-		setFocusable(true);
-	}
+    public void setAvgFps(String avgFps) {
+        this.avgFps = avgFps;
+    }
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-	}
+    public AnimationGamePanel(Context context) {
+        super(context);
+        // adding the callback (this) to the surface holder to intercept events
+        getHolder().addCallback(this);
 
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		// at this point the surface is created and
-		// we can safely start the game loop
-		thread.setRunning(true);
-		thread.start();
-	}
+        // create Elaine and load bitmap
+        elaine = new StickmanAnimated(BitmapFactory.decodeResource(getResources(), R.mipmap.running), 10, 50    // initial position
+                        , 123, 128    // width and height of sprite
+                        , 20, 7);    // FPS and number of frames in the animation
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		Log.d(TAG, "Surface is being destroyed");
-		// tell the thread to shut down and wait for it to finish
-		// this is a clean shutdown
-		boolean retry = true;
-		while (retry) {
-			try {
-				thread.join();
-				retry = false;
-			} catch (InterruptedException e) {
-				// try again shutting down the thread
-			}
-		}
-		Log.d(TAG, "Thread was shut down cleanly");
-	}
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			// handle touch
-		}
-		return true;
-	}
+        // create the game loop thread
+        thread = new MainThread(getHolder(), this);
 
-	public void render(Canvas canvas) {
-		canvas.drawColor(Color.WHITE);
-		elaine.draw(canvas);
-	}
+        // make the GamePanel focusable so it can handle events
+        setFocusable(true);
+    }
 
-	/**
-	 * This is the game update method. It iterates through all the objects
-	 * and calls their update method if they have one or calls specific
-	 * engine's update method.
-	 */
-	public void update() {
-		elaine.update(System.currentTimeMillis());
-	}
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // at this point the surface is created and
+        // we can safely start the game loop
+        thread.interrupt();
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d(TAG, "Surface is being destroyed");
+        // tell the thread to shut down and wait for it to finish
+        // this is a clean shutdown
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                // try again shutting down the thread
+            }
+        }
+        Log.d(TAG, "Thread was shut down cleanly");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            // handle touch
+        }
+        return true;
+    }
+
+    public void render(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+        elaine.draw(canvas);
+    }
+
+    /**
+     * This is the game update method. It iterates through all the objects
+     * and calls their update method if they have one or calls specific
+     * engine's update method.
+     */
+    public void update() {
+        elaine.update(System.currentTimeMillis());
+    }
 
 }
