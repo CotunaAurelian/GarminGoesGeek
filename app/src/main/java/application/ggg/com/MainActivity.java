dd06968f41ac6cc2ccbfd50bb587a98ecd4214b3
Simina, Dorin <dorin.simina@garmin.com>
2016-06-02 15:14:52 +0300

    Stickman running animated

--- app/src/main/java/application/ggg/com/MainActivity.java
+++ app/src/main/java/application/ggg/com/MainActivity.java
@@ -2,6 +2,10 @@ package application.ggg.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import application.ggg.com.running.AnimationGamePanel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout containerRunning = (RelativeLayout) findViewById(R.id.containerRunning);
+
+        AnimationGamePanel gamePanel = new AnimationGamePanel(this);
+        gamePanel.setBackground(null);
+        gamePanel.setLayoutParams(new LinearLayout.LayoutParams(170, 170));
+        containerRunning.addView(gamePanel);
+
+        //setContentView(new MainGamePanel(this));
     }
 }
-ls
+
---
+++ app/src/main/java/application/ggg/com/running/AnimationGamePanel.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package application.ggg.com.running;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import application.ggg.com.R;
+
+/**
+ * @author impaler
+ * This is the main surface that handles the ontouch events and draws
+ * the image to the screen.
+ */
+public class AnimationGamePanel extends SurfaceView implements
+		SurfaceHolder.Callback {
+
+	private static final String TAG = AnimationGamePanel.class.getSimpleName();
+	
+	private MainThread thread;
+	private StickmanAnimated elaine;
+
+	// the fps to be displayed
+	private String avgFps;
+	public void setAvgFps(String avgFps) {
+		this.avgFps = avgFps;
+	}
+
+	public AnimationGamePanel(Context context) {
+		super(context);
+		// adding the callback (this) to the surface holder to intercept events
+		getHolder().addCallback(this);
+
+		// create Elaine and load bitmap
+		elaine = new StickmanAnimated(
+				BitmapFactory.decodeResource(getResources(), R.mipmap.running)
+				, 10, 50	// initial position
+				, 123, 128	// width and height of sprite
+				, 20, 7);	// FPS and number of frames in the animation
+		
+		// create the game loop thread
+		thread = new MainThread(getHolder(), this);
+		
+		// make the GamePanel focusable so it can handle events
+		setFocusable(true);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// at this point the surface is created and
+		// we can safely start the game loop
+		thread.setRunning(true);
+		thread.start();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d(TAG, "Surface is being destroyed");
+		// tell the thread to shut down and wait for it to finish
+		// this is a clean shutdown
+		boolean retry = true;
+		while (retry) {
+			try {
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				// try again shutting down the thread
+			}
+		}
+		Log.d(TAG, "Thread was shut down cleanly");
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			// handle touch
+		}
+		return true;
+	}
+
+	public void render(Canvas canvas) {
+		canvas.drawColor(Color.WHITE);
+		elaine.draw(canvas);
+	}
+
+	/**
+	 * This is the game update method. It iterates through all the objects
+	 * and calls their update method if they have one or calls specific
+	 * engine's update method.
+	 */
+	public void update() {
+		elaine.update(System.currentTimeMillis());
+	}
+
+}
